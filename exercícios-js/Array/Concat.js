695e35cde375245f7990503b4366ff630edbbc2f
Laura <laurastephanieribeiro98@gmail.com>
2022-09-21 18:37:40 -0300

    commit funções do array

---
+++ exercícios-js/Array/Concat.js
@@ -0,0 +1,6 @@
+const filhas = ['Valeska', 'Cibalena']
+const filhos = ['Washington', 'Weslei']
+const todos = filhas.concat(filhos, 'fulano')
+console.log(todos, filhas, filhos)
+
+console.log([].concat([1, 2], [3, 4], 5, [[6, 7]]))
