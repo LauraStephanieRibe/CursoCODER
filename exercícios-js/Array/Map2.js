59b705485fc9a28d236258b1830d79ab83fbd637
Laura <laurastephanieribeiro98@gmail.com>
2022-09-21 15:23:29 -0300

    commit aprendendo sobre array

---
+++ exercícios-js/Array/Map2.js
@@ -0,0 +1,17 @@
+const carrinho = [
+    '{"nome": "Borracha", "preco": 3.45}',
+    '{"nome": "Caderno", "preco": 13.98 }',
+    '{"nome": "Kit de lapis", "preco": 41.22}',
+    '{"nome": "Caneta", "preco": 7.50}',
+]
+
+console.log(carrinho)
+
+// retornar um array apenas com os preços
+
+const paraObjeto = json => JSON.parse(json)
+const apenasPreco = produto => produto.preco
+
+const resultado = carrinho.map(paraObjeto).map(apenasPreco)
+console.log(resultado)
+
