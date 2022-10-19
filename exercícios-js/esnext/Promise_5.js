5e5a1554e3973f7fc6a051446130c7251b587fd4
Laura <laurastephanieribeiro98@gmail.com>
2022-10-19 19:55:11 -0300

    commit como e feito o tratamento de erro dentro do contexto das promise

---
+++ exercícios-js/esnext/Promise_5.js
@@ -0,0 +1,26 @@
+
+
+function funcionarOuNao(valor, chanceErro) {
+    return new Promise((resolve, reject) => {
+        try {
+            con.log('temp')
+            if (Math.random() < chanceErro) {
+                reject('Ocorreu um erro!')
+            } else {
+                resolve(valor)
+            }
+        } catch(e) {
+            reject(e)
+        }
+    })
+}
+
+funcionarOuNao('testando...', 0.8)
+    .then(v => `Valor: ${v}`)
+    .then(
+        v => consol.log(v),
+        err => console.log(`Erro Esp.: ${err}`)
+    )
+    .then(() => console.log('Quase Fim!'))
+    .catch(err => console.log(`Erro Geral: ${err}`)) // deve ser colocado no final
+    .then(() => console.log('Fim'))
